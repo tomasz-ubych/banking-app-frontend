a7cfeffc8a67ec110e446dc21ecad68aafc3ed24
qwerty <tubych@gmail.com>
2021-07-12 20:37:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      customers: [],
+    };
+  }
+  getCustomers(){
+    axios('http://localhost:8088/customer')
+    .then(response => this.setState({
+      customers : response.data._embedded.customer
+    }));
+  }
+
+  getCustomerById(id){
+    axios('http://localhost:8088/customer/{id}')
+    .then(response => this.setState({
+      customer : response
+    }));
+  }
+
+  componentWillMount(){
+   this.getCustomers();
+  }
+
+  render(){
+    return <div className="App">
+        <input type="radio" value="Pesel" name="searchParameter" /> PESEL
+        <input type="radio" value="Id" name="searchParameter"/> Id
+        
+      <table>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+        </tr>
+        {this.state.customers.map(customer => 
+    <div><td>{customer.firstName}</td> <td>{customer.lastName}</td>
+    </div>)}
+    </table>
+    </div>;
+  }
 }
+
 
 export default App;
