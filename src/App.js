3417fca80e8fb48b45622a807a0c1b97b5fe56b7
qwerty <tubych@gmail.com>
2021-07-20 22:58:39 +0200

    changed the way of displaying customers list

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ import { NavLink, Switch, Route } from 'react-router-dom';
 class App extends Component {
   constructor(props) {
     super(props)
-    this.getCustomerById = this.getCustomerById.bind(this);
+    this.getCustomer = this.getCustomer.bind(this);
     this.onInputchange = this.onInputchange.bind(this);
 
     this.state = {
       customers: [],
+      tempCustomer: '',
       id: '',
-      searchParameter: '',
+      searchParameter: 'ID',
+      iterator: 0,
     };
   }
   onInputchange(event) {
@@ -24,31 +26,55 @@ class App extends Component {
     });
   }
 
-  getCustomerById() {
-    this.setState({ customers: [] });
-    if (this.state.searchParameter == "ID") {
-      axios.get(`http://localhost:8088/customer/${this.state.id}`)
+  getCustomer() {
+    if (this.state.searchParameter === "ID") {
+      this.setState({
+          customers: []
+        })
+      axios.get(`http://localhost:8088/customers/id/${this.state.id}`)
         .then(response => this.setState({
-          customers: response.data
-        }))
+          customers: [...this.state.customers, response.data]
+        }))      
     }
     else {
-      axios.get(`http://localhost:8088/customer/search/findByPesel?pesel=${this.state.id}`)
+      axios.get(`http://localhost:8088/customers/pesel/${this.state.id}`)
         .then(response => this.setState({
           customers: response.data
         }))
+        
     }
   }
 
-
   render() {
+    if (this.state.customers[0] !== null) {
+      var tableData = this.state.customers.map(function (customer) {
+        return <tr><td>{customer.firstName}</td><td>{customer.lastName}</td><td>{customer.pesel}</td></tr>
+      });
+    }
+    let table = <table><thead></thead></table>;
+    if (this.state.customers[0] !== null && this.state.customers[0] !== undefined) {
+      table =
+        <table>
+          <thead>
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>PESEL</th>
+              <th>See details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData}
+          </tbody>
+        </table>
+    }
     return <div className="App">
       <h1>Aplikacja bankowa</h1>
       <div>
         <h1>Po czym chcesz wyszukać klienta?</h1>
         <div onChange={this.onInputchange}>
-          <input type="radio" name="searchParameter" value="PESEL" /> Pesel
-          <input type="radio" name="searchParameter" value="ID" /> Id
+          <span class="answerBottomYesNo" ><input type="radio" name="searchParameter" value="ID" defaultChecked /> Id</span>
+          <span class="answerBottomYesNo" > <input type="radio" name="searchParameter" value="PESEL" /> Pesel</span>
         </div>
         <div>
           <input name="id"
@@ -56,52 +82,13 @@ class App extends Component {
             value={this.state.id}
             onChange={this.onInputchange} />
         </div>
-        <button onClick={this.getCustomerById}> Pobierz dane klienta </button>
+        <button onClick={this.getCustomer}> Pobierz dane klienta </button>
       </div>
-
-      <table>
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-        </tr><td>{this.state.customers.firstName}</td>
-        <td>{this.state.customers.lastName}</td>
-      </table>
+      {table}
     </div>;
   }
 }
 
-const Navigation = () => (
-  <nav>
-    <ul>
-      {/*<li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li> */}
-      {/* <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li> */}
-    </ul>
-  </nav>
-);
-
-const Main = () => (
-  <Switch>
-    <Route exact path='/about' component={About}></Route>
-    <Route exact path='/contact' component={Contact}></Route>
-  </Switch>
-);
-
-
-
-const About = () => (
-  <div className='about'>
-    <h1>About Me</h1>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-  </div>
-);
-
-const Contact = () => (
-  <div className='contact'>
-    <h1>Contact Me</h1>
-    <p>You can reach me via email: <strong>hello@example.com</strong></p>
-  </div>
-);
 
 
 export default App;
